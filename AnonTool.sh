cb62332f96721b41cd917da8f6f4fc9c81c5d79e
Anon6372098 <43740735+Anon6372098@users.noreply.github.com>
2018-11-15 11:42:18 +0700

    Main File

--- AnonTool.sh
+++ AnonTool.sh
@@ -54,7 +54,7 @@ echo "13.> Xerxes (DDoS)"
 echo "[===============================================]"
 echo "14.> WhoIs LookUp"
 echo "[===============================================]"
-echo "15.> MD5 Cracker"
+echo "15.> Mining Bitcoin"
 echo "[===============================================]"
 echo "16.> NMap Android"
 echo "[===============================================]"
@@ -94,7 +94,7 @@ if [ $anon = 1 ] || [ $anon = 1 ]
 then
 clear
 figlet "Anon Tool (Gmail Hack)" | lolcat
-pkg update && upgrade
+pkg update && pkg upgrade
 pkg install python2
 pkg install figlet
 pkg install git
@@ -111,7 +111,7 @@ if
 then
 clear
 figlet "Anon Tool (Hack Facebook)" | lolcat
-pkg update && upgrade
+pkg update && pkg upgrade
 pkg install python2
 pkg install figlet
 pkg install git
@@ -126,7 +126,7 @@ fi
 if [ $anon = 3 ] || [ $anon = 3 ]
 then
 figlet "Anon Tool (Instagram)" | lolcat
-pkg update && upgrade
+pkg update && pkg upgrade
 pkg install git
 pkg install python2
 pip2 install mechanize
@@ -145,7 +145,7 @@ if [ $anon = 4 ] || [ $anon = 4 ]
 then
 clear
 figlet "Anon Tool (Hack ZIP)" | lolcat
-pkg update && upgrade
+pkg update && pkg upgrade
 pkg install python2
 pkg install figlet
 pkg install git
@@ -202,7 +202,7 @@ if [ $anon = 8 ] || [ $anon = 8 ]
 then
 clear
 figlet "Anon Tool (SQL Map)" | lolcat
-apt update && upgrade
+apt update && apt upgrade
 apt install python2
 apt install git
 https://github.com/sqlmapproject/sqlmap
@@ -292,6 +292,184 @@ pkg install wget
 wget -O tuyul.php https://pastebin.com/raw/3ERZV3nL
 mv tuyul.php $HOME
 php $HOME/tuyul.php
+fi
+
+if [ $anon = 16 ] || [ $anon = 16 ]
+then
+clear
+figlet "Anon Tool (NMap Android)" | lolcat
+pkg update && apt upgrade
+pkg install nmap
+fi
+
+if [ $anon = 17 ] || [ $anon = 17 ]
+then
+clear
+figlet "Anon Tool (Google Dorking)" | lolcat
+apt update 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/XG77Z10/Google-Dork
+mv Google-Dork $HOME
+cd $HOME/Google-Dork
+python2 Google.py
+fi
+
+if [ $anon = 18 ] || [ $anon = 18 ]
+then
+clear
+figlet "Anon Tool (LITESCRIPT)" | lolcat
+apt update 
+apt upgrade
+apt install git
+apt install perl
+git clone https://github.com/4L13199/LITESCRIPT
+mv LITESCRIPT $HOME
+cd $HOME/LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $anon = 19 ] || [ $anon = 19 ]
+then
+clear
+figlet "Anon Tool (Hydra)" | lolcat
+apt update && apt upgrade
+apt install git hydra
+git clone https://github.com/Gameye98/Black-Hydra/
+cd b-hydra
+mv blackhydra.py main.py
+chmod +x main.py
+python2 main.py
+fi
+
+if [ $anon = 20 ] || [ $anon = 20 ]
+then
+clear
+figlet "Anon Tool (Kali Nethunter)" | lolcat
+apt update && apt upgrade
+git clone https://github.com/Hax4us/Nethunter-In-Termux
+cd b-hydra
+cd nethunter
+cp kalinethunter ~
+chmod +x kalinethunter
+./kalinethunter
+fi
+
+if [ $anon = 21 ] || [ $anon = 21 ]
+then
+clear
+figlet "Anon Tool (Hash Buster)" | lolcat
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+mv Hash-Buster $HOME
+cd $HOME/Hash-Buster
+python2 hash.py
+fi
+
+if [ $anon = 22 ] || [ $anon = 22 ]
+then
+clear
+figlet "Anon Tool (D-TECT)" | lolcat
+pkg install git python2
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+python2 d-tect.py
+fi
+
+if [ $anon = 23 ] || [ $anon = 23 ]
+then
+clear
+figlet "Anon Tool (Ubuntu)" | lolcat
+apt install git
+apt install wget
+apt install proot
+git clone https://github.com/Neo-Oli/termux-ubuntu.git
+cd termux-ubuntu
+chmod +x ubuntu.sh
+./ubuntu.sh
+./startubuntu.sh
+fi
+
+if [ $anon = 24 ] || [ $anon = 24 ]
+then
+clear
+figlet "Anon Tool (viSQL)" | lolcat
+pkg update -y
+pkg install -y git
+pkg install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/blackvkng/viSQL.git
+python2 viSQL.py
+fi
+
+if [ $anon = 25 ] || [ $anon = 25 ]
+then
+clear
+figlet "Anon Tool (RouterSploit)" | lolcat
+pkg update -y
+pkg install -y git
+pkg install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/reverse-shell/routersploit.git
+cd /data/data/com.termux/files/home && cd routersploit
+pip2 install -r requirements.txt
+pip2 install -r requirements-dev.txt
+pip2 install -r requests
+python2 rsf.py
+fi
+
+if [ $anon = 26 ] || [ $anon = 26 ]
+then
+clear
+figlet "Anon Tool (AngryFuzzer)" | lolcat
+pkg update -y
+pkg install -y git
+pkg install -y python2
+cd /data/data/com.termux/files/home && git clone https://github.com/ihebski/angryFuzzer.git
+cd /data/data/com.termux/files/home && cd angryFuzzer
+cd /data/data/com.termux/files/home && pip2 install -r requirements.txt
+cd /data/data/com.termux/files/home && pip2 install requests
+cd /data/data/com.termux/files/home
+python2 angryFuzzer.py
+fi
+
+if [ $anon = 27 ] || [ $anon = 27 ]
+then
+clear
+figlet "Anon Tool (Fedora)" | lolcat
+pkg update -y
+pkg install -y git
+pkg install -y wget
+apt update && apt install wget -y && /data/data/com.termux/files/usr/bin/wget https://raw.githubusercontent.com/nmilosev/termux-fedora/master/termux-fedora.sh
+sh termux-fedora.sh f26_arm
+fi
+
+if [ $anon = 28 ] || [ $anon = 28 ]
+then
+clear
+figlet "Anon Tool (Cupp)" | lolcat
+pkg update -y
+pkg install -y git
+pkg install -y python
+cd /data/data/com.termux/files/home && git clone https://github.com/Mebus/cupp.git
+python cupp3.py
+fi
+
+if [ $anon = 29 ] || [ $anon = 29 ]
+then
+clear
+figlet "Anon Tool (Chatting)" | lolcat
+pkg install irssi
+irssi
+fi
+
+if [ $anon = 30 ] || [ $anon = 30 ]
+then
+clear
+figlet "Anon Tool (Katoolin Tool)" | lolcat
+cd anon-arsip
+python2 katoolin.py
 fi
 
 if [ $anon = 0 ] || [ $anon = 00 ]
